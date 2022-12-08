16bad185982c72ee5a510942b149f7ee8359792d
Admin <harshukani93@gmail.com>
2022-12-08 14:23:26 +0530

    commit3

--- app/src/main/java/com/ukani/resumebuilder/MainActivity.java
+++ app/src/main/java/com/ukani/resumebuilder/MainActivity.java
@@ -25,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
         mimage = findViewById(R.id.image_view);
